f7afe758effd405d4a79fef04e52782599c230ad
Jacky <luojie199403@126.com>
2017-03-11 18:56:16 +0800

    updata

--- build/generated/source/buildConfig/debug/com/example/answer/BuildConfig.java
+++ build/generated/source/buildConfig/debug/com/example/answer/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example.answer;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.example.answer";
+  public static final String APPLICATION_ID = "huas.fur.Answer";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
